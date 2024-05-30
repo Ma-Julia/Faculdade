23087254925ab663727b6f66bce1a3f30dabfbec
Nailson Gomes <48133821+nailsongomesdk@users.noreply.github.com>
2024-05-30 17:15:08 -0300

    criando projeto

---
+++ CampSoft/CampSoft/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CampSoft
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CampSoft/CampSoft/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CampSoft
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
