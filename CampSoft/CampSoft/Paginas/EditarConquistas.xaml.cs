297ab42a8f5f3373e2678871d714f8fd602ca6df
Maria Julia <majuliacavalcanti@gmail.com>
2024-06-16 16:25:37 -0300

    Tela de editar conquistas

--- CampSoft/CampSoft/Paginas/EditarConquistas.xaml.cs
+++ CampSoft/CampSoft/Paginas/EditarConquistas.xaml.cs
@@ -6,4 +6,19 @@ public partial class EditarConquistas : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void DataEvento_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        
+    }
+
+    private async void BTNVoltar_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopModalAsync();
+    }
+
+    private void BTNOK_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
